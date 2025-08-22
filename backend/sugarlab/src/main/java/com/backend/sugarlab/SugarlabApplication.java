4e8f0d3c7f20ee157d3a9eecab06b22a672c9739
Pedro Vitoreti <133826934+PHV00@users.noreply.github.com>
2025-08-22 23:19:47 +0000

    feat: start backend spring boot

---
+++ backend/sugarlab/src/main/java/com/backend/sugarlab/SugarlabApplication.java
@@ -0,0 +1,13 @@
+package com.backend.sugarlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugarlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugarlabApplication.class, args);
+	}
+
+}
---
+++ backend/sugarlab/src/test/java/com/backend/sugarlab/SugarlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.sugarlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugarlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
