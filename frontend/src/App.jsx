de4b2b293e232a4fc3a5ee1e3327a92fd317f7f1
Gustavsonpy <gupaluski@gmail.com>
2025-09-09 20:10:40 -0300

    Footer criado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
 import Header from './components/header/Header'
+import Footer from './components/Footer/Footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
       <Header/>
+      <Footer/>
+      {/* <BrowserRouter>
+        <Header/>
+        <Routes>
+        </Routes>
+      </BrowserRouter> */}
     </>
   )
 }
