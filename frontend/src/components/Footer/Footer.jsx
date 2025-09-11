52118970c328df2d9371ab5b57f44d534832b883
Gustavo <gupaluski@gmail.com>
2025-09-11 17:49:47 -0300

    fix: Making the footer responsive

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,14 +4,14 @@ import logo_white from '../../assets/image/SugarLabBranco.png';
 
 const Footer = () => {
     return (
-        <div id="footer" className='flex justify-center items-center'>
+        <div id="footer" className='flex justify-center items-center py-2'>
             <div id="content-container">
-                <div className="texts flex justify-between">
-                    <div id="links" className='flex gap-12'>
+                <div className="texts flex flex-col gap-2 items-center md:justify-between md:flex-row">
+                    <div id="links" className='flex flex-col gap-0 md:gap-12 md:flex-row'>
                         <NavLink to={"/"} className='text-white'>Comunidade</NavLink>
                         <NavLink to={"/"} className='text-white'>Empresa</NavLink>
                     </div>
-                    <img src={logo_white} alt="logo" className='w-1/12'/>
+                    <img src={logo_white} alt="logo" className='w-1/6 md:w-1/12'/>
                 </div>
                 <hr className='border-white'/>
             </div>
