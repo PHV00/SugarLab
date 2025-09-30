bf217318d08f3c88023ede7553ecc0e23e7640db
danisantosss <danisantos5075@gmail.com>
2025-09-30 20:25:54 -0300

    feat: login service + ajuste UserRepository

---
+++ backend/sugarlab/src/main/java/com/backend/sugarlab/service/LoginService.java
@@ -0,0 +1,28 @@
+package com.backend.sugarlab.service;
+
+import com.backend.sugarlab.DTO.LoginRequestDTO;
+import com.backend.sugarlab.entity.Usuario;
+import com.backend.sugarlab.repository.UserRespository;
+
+public class LoginService {
+    
+    private final UserRespository userRespository;
+
+    public LoginService(UserRespository userRespository){
+        this.userRespository = userRespository;
+    }
+
+    public String login(LoginRequestDTO loginRequest) {
+        Usuario usuario = userRespository.findByEmail(loginRequest.getEmail());
+
+        if (usuario == null) {
+            return "Usuário não encontrado";
+        }
+        
+        if (!usuario.getSenha().equals(loginRequest.getSenha())) {
+            return "Senha Incorreta";
+        }
+
+        return "Login Realizado com Sucesso!";
+    }
+}
