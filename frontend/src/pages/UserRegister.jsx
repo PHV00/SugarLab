b0bf794819ff5253929d6e8cb832b3c7c7ae4825
AmandKorcz <amanda.korczagin@gmail.com.br>
2025-10-03 17:46:55 -0300

    feat: registro de novo usuário

---
+++ frontend/src/pages/UserRegister.jsx
@@ -0,0 +1,40 @@
+import './UserRegister.css'
+import Logo from "../assets/image/SugarLab.png"
+
+export default function UserRegister() {
+    return(
+        <div className='register-container'>
+            <main className='register-main'>
+                <section className='register-intro'>
+                    <h1>Conecte-se</h1>
+                    <p>
+                        E desbloqueie um mundo de sabores! Acesse os mais diversos cursos de culinária 
+                        da atualidade e transfome sua paixão por conzinhar em habilidades extraordinárias.
+                    </p>
+                </section>
+
+                <section className='register-card'>
+                    <img 
+                        src={Logo}
+                        alt="Sugar Lab Logo"
+                        className="register-logo"
+                    />
+
+                    <form className='register-form' onSubmit={(e) => e.preventDefault()}>
+                        <div className='row'>
+                            <input type='text' placeholder='Nome'/>
+                            <input type='text' placeholder='Sobrenome'/>
+                        </div>
+
+                        <input type='email' placeholder='Email'/>
+                        <input type='password' placeholder='Senha'/>
+
+                        <button type='submit'>Criar</button>
+
+                    </form>
+                </section>
+
+            </main>
+        </div>
+    )
+}
