76be52697b6c7b4aff23d6810d600b97dbcc50ad
AmandKorcz <amanda.korczagin@gmail.com.br>
2025-10-05 10:48:31 -0300

    feat: adicionando sobre-nós

---
+++ frontend/src/pages/AboutUs.jsx
@@ -0,0 +1,44 @@
+import "./AboutUs.css"
+import CatolicaLogo from "../assets/image/CatolicaLogo.png"
+
+export default function AboutUs() {
+    return (
+        <div className="about">
+            <main className="about-card">
+                <h1>Bem-vindo ao SugarLab!</h1>
+                <p>
+                    Nossa missão é democratizar o acesso ao conhecimento, oferecendo cursos acessíveis, 
+                    didáticos e de alta qualidade para todos que desejam explorar o mundo da confeitaria,
+                    seja como hobbie ou profissão. 
+                </p>
+
+                <div className="about-info">
+                    <div className="about-left">
+                        <h2>Equipe de desenvolvedores:</h2>
+                        <ul className="about-team">
+                            <li>AMANDA KORCZAGIN</li>
+                            <li>DANIEL DOUGLAS DOS SANTOS</li>
+                            <li>FLAVIA ANTONIELI DE SOUZA</li>
+                            <li>GUILHERME MAFRA PALUSKI</li>
+                            <li>GUSTAVO MAFRA PALUSKI</li>
+                            <li>PEDRO HENRIQUE VITORETI</li>
+                        </ul>
+
+                        <h3>Orientador do projeto:</h3>
+                        <ul className="about-orientador">
+                            <li>LUIZ CARLOS CAMARGO</li>
+                        </ul>
+                    </div>
+
+                    <div className="about-right">
+                        <img
+                            src={CatolicaLogo}
+                            alt="Centro Universitário Católica de Santa Catarina"
+                            className="about-logo"
+                        />
+                    </div>
+                </div>
+            </main>
+        </div> 
+    )
+}
