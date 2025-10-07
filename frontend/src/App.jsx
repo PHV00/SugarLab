2366b12ff20e53b3dfef9ec5817238fcef1f54f2
AmandKorcz <amanda.korczagin@gmail.com.br>
2025-10-06 21:11:40 -0300

    feat: signature routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import AboutUs from './pages/AboutUs'
 import Footer from './components/Footer/Footer'
 import { Routes, Route } from 'react-router-dom'
 import UserRegister from './pages/UserRegister'
+import Subscription from './pages/Subscription'
 function App() {
 
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path='/cursos' element={<Courses/>}/>
         <Route path='/registro' element={<UserRegister/>}/>
         <Route path='/sobre' element={<AboutUs/>}/>
+        <Route path='/assinatura' element={<Subscription/>}/>
       </Routes>
 
       <Footer/>
