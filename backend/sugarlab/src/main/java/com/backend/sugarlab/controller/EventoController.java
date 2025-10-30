78f5a360c1df0c0366e8cedc5057a2a39342890c
danisantosss <danisantos5075@gmail.com>
2025-10-29 21:42:09 -0300

    feat: controller createEvento

--- backend/sugarlab/src/main/java/com/backend/sugarlab/controller/EventoController.java
+++ backend/sugarlab/src/main/java/com/backend/sugarlab/controller/EventoController.java
@@ -1,10 +1,31 @@
 package com.backend.sugarlab.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.sugarlab.DTO.EventoDTO;
+import com.backend.sugarlab.entity.Evento;
+import com.backend.sugarlab.repository.EventoRepository;
+import com.backend.sugarlab.service.EventoService;
 
 @RestController
 @RequestMapping("/eventos/v1")
 public class EventoController {
     
+    @Autowired
+    EventoRepository eventoRepository;
+
+    @Autowired
+    EventoService eventoService;
+
+    @PostMapping("/newEvento")
+    public ResponseEntity<Evento> createEvento(@RequestBody EventoDTO dto){
+        Evento novoEvento = eventoService.criarEvento(dto);
+
+        return ResponseEntity.ok().body(novoEvento);
+    }
 }
