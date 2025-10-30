aa2f5d053af4e569a49355114239699bffb1209d
danisantosss <danisantos5075@gmail.com>
2025-10-29 21:50:19 -0300

    feat: Evento Controller

--- backend/sugarlab/src/main/java/com/backend/sugarlab/controller/EventoController.java
+++ backend/sugarlab/src/main/java/com/backend/sugarlab/controller/EventoController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +38,17 @@ public class EventoController {
     @GetMapping("/eventos")
     public ResponseEntity<List<Evento>> getAllEventos(){
         return ResponseEntity.ok().body(eventoService.listarEventos());
+    }
+
+    @PutMapping("eventos/edit/{id}")
+    public ResponseEntity<Evento> editEvento(@PathVariable int id, @RequestBody EventoDTO dto){
+        try {
+            Evento newEvento = eventoService.editarEvento(id, dto);
+
+            return ResponseEntity.ok(newEvento);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping("/eventos/{id}")
