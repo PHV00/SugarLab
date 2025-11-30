4554b05499b58d32c18c52a438ef2f578d05ca64
PHV00 <vitoretipedro@gmail.com>
2025-11-30 20:46:55 -0300

    merge

--- frontend/src/pages/admin/AdminRoute.jsx
+++ frontend/src/pages/admin/AdminRoute.jsx
@@ -1,10 +1,8 @@
 import { Navigate } from "react-router-dom";
-import { jwtDecode } from "jwt-decode";
 
 export default function AdminRoute({ children }) {
   const role = localStorage.getItem("role");
   const token = localStorage.getItem("token");
-  console.log("Token no AdminRoute:", token);
 
   if (!token) return <Navigate to="/login" />;
 
